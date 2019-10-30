bf9fc01361b6df79f39a0c78c0d5ca6c2f8a29c9
Emma Dirnberger <emmadirnbergegmail.com>
2019-10-30 15:08:01 +0100

    First task solution, first version

--- src/InventoryManagementSystem.java
+++ src/InventoryManagementSystem.java
@@ -1,4 +1,83 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class InventoryManagementSystem {
+	private int checkSum;
+	private int numberOfDoubles;
+	private int numberOfTriples;
+	
+	InventoryManagementSystem() {
+		checkSum = 0;
+		numberOfDoubles = 0;
+		numberOfTriples = 0;
+	}
+	
+	private ArrayList<Character> turnStringToList(String stringToConvert) {
+		ArrayList<Character> listOfChars = new ArrayList<Character>();
+		
+        for (char ch : stringToConvert.toCharArray()) { 
+        	  
+        	listOfChars.add(ch); 
+        } 
+        
+        return listOfChars;
+	}
+	
+	public void checkInputForDoublesAndTriples(ArrayList<String> input) {
+		
+		for(String string: input) {
+			ArrayList<Character> chars = turnStringToList(string);
+			checkTriplesAndDoubles(chars);
+		}
+		setCheckSum(getNumberOfDoubles(), getNumberOfTriples());
+	}
+	
+	public void checkTriplesAndDoubles(ArrayList<Character> chars) {
+		ArrayList<Character> triples = new ArrayList<Character>();
+		ArrayList<Character> doubles = new ArrayList<Character>();
+		boolean gotDoubles = false;
+		boolean gotTriples = false;
+		
+		for (int i = 0; i < chars.size(); i++) {
+			
+			if ( (Collections.frequency(chars, chars.get(i))) == 3 ){
+				triples.add(chars.get(i));   
+			    gotTriples = true;
+			}
+			
+			else if ( (Collections.frequency(chars, chars.get(i))) == 2 ){
+				doubles.add(chars.get(i));		    
+			    gotDoubles = true;
+			}
+			else {
+				
+			}
+			
+		}
+		
+		if (gotDoubles) {
+			numberOfDoubles++;
+		}
+		
+		if (gotTriples) {
+			numberOfTriples++;
+		}
+	}
+	
+	private void setCheckSum(int numberOfDoubles, int numberOfTriples) {
+		checkSum = numberOfDoubles * numberOfTriples;
+	}
+	
+	public int getNumberOfDoubles() {
+		return numberOfDoubles;
+	}
+	
+	public int getNumberOfTriples() {
+		return numberOfTriples;
+	}
+	
+	public int getCheckSum() {
+		return checkSum;
+	}
 
 }
--- src/InventoryManagementSystemTest.java
+++ src/InventoryManagementSystemTest.java
@@ -1,12 +1,98 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import org.junit.jupiter.api.Test;
 
 class InventoryManagementSystemTest {
+	
+	private ArrayList<String> readInputAsArrayOfStrings() throws FileNotFoundException{
+		Scanner s = new Scanner(new File("InventoryManagementSystemInput.txt"));
+	    ArrayList<String> list = new ArrayList<String>();
+	    
+	    while (s.hasNext()) {
+	        list.add(s.next());
+	    }
+	    s.close();
+		
+	    return list;
+	}
+	
+	private ArrayList<Character> turnStringToList(String stringToConvert) {
+		ArrayList<Character> listOfChars = new ArrayList<Character>();
+		
+        for (char ch : stringToConvert.toCharArray()) { 
+        	  
+        	listOfChars.add(ch); 
+        } 
+        
+        return listOfChars;
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void no_doubles_or_triples() {
+		InventoryManagementSystem inventory = new InventoryManagementSystem();
+		String testCode = "abcdef";
+		ArrayList<Character> chars = turnStringToList(testCode);
+		inventory.checkTriplesAndDoubles(chars);
+		
+		
+		assert((inventory.getNumberOfDoubles() == 0 && inventory.getNumberOfTriples() == 0));
+	}
+	
+	@Test
+	void contains_doubles_no_triples() {
+		InventoryManagementSystem inventory = new InventoryManagementSystem();
+		String testCode = "abbcde";
+		ArrayList<Character> chars = turnStringToList(testCode);
+		inventory.checkTriplesAndDoubles(chars);
+		
+		
+		assert(inventory.getNumberOfDoubles() > 0 && inventory.getNumberOfTriples() == 0);
+	}
+	
+	@Test
+	void contains_triples_no_doubles() {
+		InventoryManagementSystem inventory = new InventoryManagementSystem();
+		String testCode = "abcccd";
+		ArrayList<Character> chars = turnStringToList(testCode);
+		inventory.checkTriplesAndDoubles(chars);
+		
+		
+		assert(inventory.getNumberOfDoubles() == 0 && inventory.getNumberOfTriples() > 0);
+	}
+	
+	@Test
+	void total_checkSum_for_arrayList_of_strings() {
+		InventoryManagementSystem inventory = new InventoryManagementSystem();
+		ArrayList<String> myTestList = new ArrayList<String>();
+		
+		myTestList.add("abcdef");
+		myTestList.add("bababc");
+		myTestList.add("abbcde");
+		myTestList.add("abcccd");
+		myTestList.add("aabcdd");
+		myTestList.add("abcdee");
+		myTestList.add("ababab");
+		
+		inventory.checkInputForDoublesAndTriples(myTestList);
+		
+		
+		assert(inventory.getNumberOfDoubles() > 0 && inventory.getNumberOfTriples() > 0);
+		assert(inventory.getCheckSum() == 12);
+	}
+	
+	@Test
+	void file_input() throws FileNotFoundException {
+		InventoryManagementSystem inventory = new InventoryManagementSystem();
+		ArrayList<String> myTestInput = readInputAsArrayOfStrings();
+		inventory.checkInputForDoublesAndTriples(myTestInput);
+		
+		assert(inventory.getNumberOfDoubles() > 0 && inventory.getNumberOfTriples() > 0);
+		assert(inventory.getCheckSum() == 5880);
 	}
 
 }
